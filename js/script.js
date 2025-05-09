8e0b99791cd3cd904fc92f99990f28af9daa8931
juan david <dabson753@gmail.com>
2025-05-08 23:02:36 -0600

    Se agrego diseño en recursos y se adapto el submenu a la mayoria de dispositivos.

--- js/script.js
+++ js/script.js
@@ -4,9 +4,11 @@
 const btnBurg = document.getElementById('btnHam');
 // Contenedor que se activara al presionar el boton hamburguesa'
 const contSub = document.getElementById('contOpc');
+const versi = document.getElementById('versiculo')
 
 btnBurg.addEventListener('click', () => {
     contSub.classList.toggle('active')
+    versi.classList.toggle('active')
 })
 
 // Esta funcionalidad es para abrir otro sitio web solo con un contenedor
@@ -15,3 +17,24 @@ const btnRecu = document.getElementById('btnRecu')
 btnRecu.addEventListener('click', () =>{
     window.location.href = '/html/recursos.html'
 })
+
+// El siguiente bloque es para que al desplazar cierta distancia se le de un fondo a la cabecera
+
+const contCabe = document.getElementById('cabecera');
+const coloRed1 = document.getElementById('color')
+const coloRed2 = document.getElementById('color2')
+
+
+window.addEventListener('scroll', () =>{
+    if(window.scrollY > 50){
+        contCabe.classList.add('con-fondo')
+        coloRed1.classList.add('con-color')
+        coloRed2.classList.add('con-color')
+    }else{
+        contCabe.classList.remove('con-fondo')
+        coloRed1.classList.remove('con-color')
+        coloRed2.classList.remove('con-color')
+    }
+})
+
+
