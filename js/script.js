8b0536ba855433f837151a54bcf624c6fd7681e9
juan david <dabson753@gmail.com>
2025-05-08 19:49:15 -0600

    Creacion de la subcabecera y funcionalidades de esta, adaptabilidad de la cabecera en varios dispositivos

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-// Hola
+// Esta primera seccion de codigo hara que funcione el boton de hamburgesa.
+// Estas son es la variable para el boton
+
+const btnBurg = document.getElementById('btnHam');
+// Contenedor que se activara al presionar el boton hamburguesa'
+const contSub = document.getElementById('contOpc');
+
+btnBurg.addEventListener('click', () => {
+    contSub.classList.toggle('active')
+})
+
+// Esta funcionalidad es para abrir otro sitio web solo con un contenedor
+const btnRecu = document.getElementById('btnRecu')
+
+btnRecu.addEventListener('click', () =>{
+    window.location.href = '/html/recursos.html'
+})
